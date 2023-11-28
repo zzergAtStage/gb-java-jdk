128d4f98b42a5a9e1907ad677c53c59e085df374
zzergAtStage <hightemplar98@iclod.com>
2023-11-28 12:15:54 +0100

    s02 announces and logging

--- src/main/java/com/zergatstage/lessons/chat/model/ChatServer.java
+++ src/main/java/com/zergatstage/lessons/chat/model/ChatServer.java
@@ -6,5 +6,23 @@ public class ChatServer {
 
     public ChatServer(ChatServerListener listener) {
         this.listener = listener;
+        this.isServerWorking = false;
+    }
+
+    public void start(){
+        if (isServerWorking){
+            listener.onMessageReceived("server is already working.");
+            return;
+        }
+        listener.onMessageReceived("Server started.");
+        isServerWorking = true;
+    }
+    public void stop(){
+        if (!isServerWorking){
+            listener.onMessageReceived("server is already stopped.");
+            return;
+        }
+        listener.onMessageReceived("Server stopped.");
+        isServerWorking = false;
     }
 }
--- src/main/java/com/zergatstage/lessons/chat/model/ChatServerListener.java
+++ src/main/java/com/zergatstage/lessons/chat/model/ChatServerListener.java
@@ -1,5 +1,5 @@
 package com.zergatstage.lessons.chat.model;
 
 public interface ChatServerListener {
-    void getMessage(Message message);
+    void onMessageReceived(String message);
 }
